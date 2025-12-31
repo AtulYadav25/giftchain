1b30d6058624513b98fe124639f59e0316c2c65b
Atul <atul007414@gmail.com>
2025-12-31 20:51:49 +0530

    update: Added Rate Limit to the api endpoints and validator to validate if file upload (image) must be less then 5 MB

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,10 @@ import fastify from 'fastify';
 import cors from '@fastify/cors';
 import multipart from '@fastify/multipart';
 import cookie from '@fastify/cookie';
+import rateLimit from '@fastify/rate-limit';
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 import { errorHandler } from './middlewares/errorHandler';
+
 import { authRoutes } from './routes/auth.routes';
 import { giftRoutes } from './routes/gift.routes';
 import { wrapperRoutes } from './routes/wrapper.routes';
@@ -12,15 +14,31 @@ import { priceRoutes } from './routes/price.routes';
 
 const app = fastify({ logger: false });
 
+// 🔒 Rate Limit (GLOBAL)
+app.register(rateLimit, {
+    max: 60,
+    timeWindow: '1 minute',
+    keyGenerator: (req) =>
+        req.user?.address || req.cookies?.session || req.ip,
+});
+
 // Plugins
 app.register(cors, {
     origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
 });
-app.register(multipart);
+
+app.register(multipart, {
+    limits: {
+        fileSize: 5 * 1024 * 1024,
+        files: 1,
+        fields: 10,
+    },
+});
+
 app.register(cookie, {
-    secret: process.env.COOKIE_SECRET, // for signed cookies
+    secret: process.env.COOKIE_SECRET,
     hook: 'onRequest'
 });
 
