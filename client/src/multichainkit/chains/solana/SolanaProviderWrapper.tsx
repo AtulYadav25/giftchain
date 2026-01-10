509012d19e83a1bf54a64c325ac83df0d9c0770c
Atul <atul007414@gmail.com>
2026-01-10 21:58:18 +0530

    Switched From SOLANA PRC to PUBLIC Node RPC

--- client/src/multichainkit/chains/solana/SolanaProviderWrapper.tsx
+++ client/src/multichainkit/chains/solana/SolanaProviderWrapper.tsx
@@ -1,16 +1,22 @@
 
 import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+// import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
+// import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const SolanaProviderWrapper = ({ children }: { children: React.ReactNode }) => {
     // You can also use 'mainnet-beta'
-    const network = WalletAdapterNetwork.Mainnet;
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+    // const network = WalletAdapterNetwork.Mainnet;
+    const endpoint = useMemo(
+        () => `https://solana-rpc.publicnode.com`,
+        []
+    );
+
+    // https://mainnet.helius-rpc.com/?api-key=c87d92e9-3b4e-4c58-9f70-ac683af109eb
+
 
     const wallets = useMemo(
         () => [new PhantomWalletAdapter()],
